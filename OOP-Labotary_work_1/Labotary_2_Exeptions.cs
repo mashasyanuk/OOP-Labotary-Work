3045584358db3164a2afac30279623f77ae5dabc
mashasyanuk <cool.super-masa@ya.ru>
2022-04-21 19:12:10 +0300

    Ex. 2 done. Text/bin files, strings

---
+++ OOP-Labotary_work_1/Labotary_2_Exeptions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Exeptions {
+    public void Analyziz() {
+
+        for (int i = -200; i <= 200; i++)
+        {
+            double current = i / 2.0;
+            Check(current);
+        }
+
+    }
+
+    public class SqrtException : Exception
+    {
+        public void Print()
+        {
+            Console.WriteLine("Корень меньше нуля");
+        }
+    }
+
+    public class InfinityException : Exception
+    {
+        public void Print()
+        {
+            Console.WriteLine("Бесконечность");
+        }
+    }
+
+
+    public void Check(double x){
+
+        try
+        {
+            double y = (x+4)/(Math.Pow(x, 2)-2) + Math.Pow((Math.Pow(x, 3)-1),0.5);
+            
+            if ((Math.Pow(x, 3) - 1) < 0)
+            {
+                throw new SqrtException();
+            }
+            if ((Math.Pow(x, 2)-1)<0)
+            {
+                throw new InfinityException();
+            }
+            Console.WriteLine($"x={x}, y={y}");
+
+        }
+        catch
+        {
+            Console.WriteLine("Возникло исключение!");
+        }
+    }
+
+}
+
+class Checking_Out {
+    public static void Main(string[] args)
+    {
+        Exeptions func = new Exeptions();
+        func.Analyziz();
+    }
+
+}
